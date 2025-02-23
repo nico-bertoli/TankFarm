2923807e22c70682f0e74c7780c8a2a4a18cd788
nico-bertoli <nicolo.bertoli@miniclip.com>
2025-02-23 17:09:54 +0100

    Initial commit

---
+++ Source/TankFarm/TankFarmGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankFarmGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKFARM_API ATankFarmGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
